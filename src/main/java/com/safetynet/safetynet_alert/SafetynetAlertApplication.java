e9df2bb4b35b46d3bb50cdcbe38c0a35611cf892
Guillaume BUJON <gbujon@greta.local>
2024-12-18 14:48:37 +0100

    Init

---
+++ src/main/java/com/safetynet/safetynet_alert/SafetynetAlertApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.safetynet_alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetynetAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetynetAlertApplication.class, args);
+	}
+
+}
