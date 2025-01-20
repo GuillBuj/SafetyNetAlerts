ffd9a392ae201cb614160769514c49e219e40db9
GuillBuj <guillaume.bujon@gmail.com>
2025-01-20 16:05:32 +0100

    getPersonsByAddressTest

--- src/test/java/com/safetynet/safetynet_alert/service/FireStationServiceTest.java
+++ src/test/java/com/safetynet/safetynet_alert/service/FireStationServiceTest.java
@@ -4,7 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,13 +21,16 @@ import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.safetynet.safetynet_alert.data.DatasTest;
 import com.safetynet.safetynet_alert.model.Datas;
+import com.safetynet.safetynet_alert.model.MedicalRecord;
 import com.safetynet.safetynet_alert.model.Person;
 import com.safetynet.safetynet_alert.repository.DataRepository;
 
 import dto.ChildAlertChildDTO;
 import dto.ChildAlertResponse;
+import dto.FireResponse;
 import dto.FireStationPersonDTO;
 import dto.FireStationResponse;
+import dto.PersonWithMedicalDTO;
 
 @ExtendWith(MockitoExtension.class)
 public class FireStationServiceTest {
@@ -99,6 +106,59 @@ public class FireStationServiceTest {
         //--THEN
         assertEquals(expectedSet, actualSet);
     }
+
+    @Test
+    void getPersonsByAddressTest(){
+        
+        //--GIVEN
+        FireResponse expectedResponse = new FireResponse(
+            Set.of(
+                new PersonWithMedicalDTO("Jackson", "Harris", "123-456-7910", 30, List.of("Aspirin"), List.of("Dust","Milk")),
+                new PersonWithMedicalDTO("Harper", "Harris", "123-456-7911", 30, List.of("Metformin", "Lisinopril"), List.of("Eggs", "Wheat"))
+            ),
+            3);
+
+        //--WHEN
+        FireResponse actualResponse = fireStationService.getPersonsByAddress("505 Oak St");
+
+        //--THEN
+        assertEquals(expectedResponse, actualResponse);
+    } 
+
+/*public Set<PersonWithMedicalDTO> getPersonSetByAddress(String address){
+        logger.debug("Getting persons(set) by address({})", address);
+
+        Datas datas = dataRepository.readData();
+        PersonService personService = new PersonService(dataRepository);
+
+        Set<Person> persons = datas.getPersons().stream()
+            .filter(person -> person.getAddress().equals(address))
+            .collect(Collectors.toSet());
+
+        Map<Person, MedicalRecord> personsMap = personService.mapPersonToMedicalRecord(persons);
+
+        Set<PersonWithMedicalDTO> personsDTO = personsMap.entrySet().stream()
+            .map(entry -> {
+                    return new PersonWithMedicalDTO(
+                        entry.getKey().getFirstName(),
+                        entry.getKey().getLastName(),
+                        entry.getKey().getPhone(),
+                        personService.getAge(entry.getKey()),
+                        entry.getValue().getMedications(),
+                        entry.getValue().getAllergies());
+            })
+            .filter(Objects::nonNull)
+            .collect(Collectors.toSet());
+
+        return personsDTO;
+    }
+    
+    public FireResponse getPersonsByAddress(String address){
+        logger.debug("Getting persons by address({})", address);
+
+        logger.info("List of people living at {}: ", address, getPersonSetByAddress(address));
+        return new FireResponse(getPersonSetByAddress(address), getStationByAddress(address));
+    }*/
 
     /* -----------------------------------TEMPLATE
     @Test
