4f112beda36452566b18201882c90dee1f3a1e4b
GuillaumeBuj <guillaume.bujon@gmail.com>
2025-01-24 16:31:05 +0100

    config json test + FirestationControllerTestIT CUD

--- src/main/java/com/safetynet/safetynet_alert/repository/DataRepository.java
+++ src/main/java/com/safetynet/safetynet_alert/repository/DataRepository.java
@@ -1,47 +1,115 @@
 package com.safetynet.safetynet_alert.repository;
 
-import java.io.File;
 import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Repository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.safetynet.safetynet_alert.exception.DataRepositoryException;
 import com.safetynet.safetynet_alert.model.Datas;
 
+// @Repository
+// public class DataRepository {
+
+//     private static final Logger logger = LogManager.getLogger(DataRepository.class);
+
+//     private static final String DATA_FILE_PATH = "src\\main\\resources\\data.json";
+//     private final ObjectMapper objectMapper;
+
+//     public DataRepository(ObjectMapper objectMapper){
+
+//         this.objectMapper = objectMapper;
+//     }
+
+//     public Datas readData() throws DataRepositoryException{
+//         try{
+//             logger.debug("Read objects from json file");
+//             return objectMapper.readValue(new File(DATA_FILE_PATH), Datas.class);
+//         } catch(IOException e){
+//             logger.error("Error reading data from file");
+//             throw new DataRepositoryException("Failed to read data from file");
+//         }
+//     }
+
+//     public void writeData(Datas datas) throws DataRepositoryException{
+//         try {
+//             logger.debug("Write objects to json file");
+//             objectMapper.writeValue(new File(DATA_FILE_PATH), datas);
+//         } catch (IOException e) {
+//             logger.error("Error writing data to file");
+//             throw new DataRepositoryException("Failed to write data to file");
+//         }
+//     }
+//
+//}
+
 @Repository
 public class DataRepository {
 
     private static final Logger logger = LogManager.getLogger(DataRepository.class);
 
-    private static final String DATA_FILE_PATH = "src\\main\\resources\\data.json";
+    @Value("${data.file.path}") // Chemin du fichier injecté depuis application.properties
+    private String dataFilePath;
+
     private final ObjectMapper objectMapper;
+    private final ResourceLoader resourceLoader;
 
-    public DataRepository(ObjectMapper objectMapper){
-        
+    public DataRepository(ObjectMapper objectMapper, ResourceLoader resourceLoader) {
         this.objectMapper = objectMapper;
+        this.resourceLoader = resourceLoader;
     }
 
-    public Datas readData() throws DataRepositoryException{
-        try{
-            logger.debug("Read objects from json file");
-            return objectMapper.readValue(new File(DATA_FILE_PATH), Datas.class);
-        } catch(IOException e){
-            logger.error("Error reading data from file");
-            throw new DataRepositoryException("Failed to read data from file");
-        }
-    }
+    // public Datas readData() throws DataRepositoryException {
+    // try {
+    // logger.debug("Reading data from file: {}", dataFilePath);
+    // Resource resource = resourceLoader.getResource(dataFilePath);
+    // return objectMapper.readValue(resource.getInputStream(), Datas.class);
+    // } catch (IOException e) {
+    // logger.error("Error reading data from file: {}", dataFilePath, e);
+    // throw new DataRepositoryException("Failed to read data from file");
+    // }
+    // }
 
-    public void writeData(Datas datas) throws DataRepositoryException{
+    public void writeData(Datas datas) throws DataRepositoryException {
         try {
-            logger.debug("Write objects to json file");
-            objectMapper.writeValue(new File(DATA_FILE_PATH), datas);
+            logger.debug("Writing data to file: {}", dataFilePath);
+            Resource resource = resourceLoader.getResource(dataFilePath);
+            objectMapper.writeValue(resource.getFile(), datas);
         } catch (IOException e) {
-            logger.error("Error writing data to file");
+            logger.error("Error writing data to file: {}", dataFilePath, e);
             throw new DataRepositoryException("Failed to write data to file");
         }
     }
 
+    public Datas readData() throws DataRepositoryException {
+        try {
+            logger.debug("Reading data from file: {}", dataFilePath);
+            System.out.println("Reading data from file: " + dataFilePath); // Ajout de syso
+
+            Resource resource = resourceLoader.getResource(dataFilePath);
+
+            logger.debug("Resource exists: {}", resource.exists()); // Vérifie si la ressource existe
+            System.out.println("Resource exists: " + resource.exists()); // Ajout de syso
+
+            logger.debug("Resource URI: {}", resource.getURI()); // Affiche l'URI de la ressource
+            System.out.println("Resource URI: " + resource.getURI()); // Ajout de syso
+
+            return objectMapper.readValue(resource.getInputStream(), Datas.class);
+        } catch (IOException e) {
+            logger.error("Error reading data from file: {}", dataFilePath, e);
+            System.out.println("Error reading data from file: " + dataFilePath); // Ajout de syso
+            e.printStackTrace(); // Ajout de la stack trace pour plus de détails
+            throw new DataRepositoryException("Failed to read data from file");
+        }
+    }
+
+    public void clear() {
+        Datas datas = new Datas();
+        writeData(datas);
+    }
 }
--- src/test/java/com/safetynet/safetynet_alert/controller/FireStationControllerIT.java
+++ src/test/java/com/safetynet/safetynet_alert/controller/FireStationControllerIT.java
@@ -1,5 +1,7 @@
 package com.safetynet.safetynet_alert.controller;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyList;
@@ -42,6 +44,7 @@ import com.safetynet.safetynet_alert.exception.AlreadyExistsException;
 import com.safetynet.safetynet_alert.exception.NotFoundException;
 import com.safetynet.safetynet_alert.model.FireStation;
 import com.safetynet.safetynet_alert.model.Person;
+import com.safetynet.safetynet_alert.repository.DataRepository;
 import com.safetynet.safetynet_alert.service.FireStationService;
 
 @SpringBootTest
@@ -51,233 +54,225 @@ public class FireStationControllerIT {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private FireStationService fireStationService;
+    @Autowired
+    private DataRepository dataRepository;
 
+    @Autowired
     private ObjectMapper objectMapper;
 
     @BeforeEach
-    public void setUp(){
-        objectMapper = new ObjectMapper();
+    void setUp() {
+        dataRepository.clear();
     }
 
-    //TODO voir si il faut faire les tests avec MethodArgumentNotValidException pour tout
-
     @Test
-    public void createFireStationTestCreated() throws JsonProcessingException, Exception{
+    public void createFireStationTestCreated() throws JsonProcessingException, Exception {
         FireStation fireStation = new FireStation("3 Rory Rd", 8);
-
-        doNothing().when(fireStationService).createFireStation(any(FireStation.class));
 
         mockMvc.perform(post("/firestation")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(fireStation)))
-                .andExpect(status().isCreated());
-        
-        verify(fireStationService, times(1)).createFireStation(fireStation);
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        assertTrue(dataRepository.readData().getFireStations().contains(fireStation));
     }
 
     @Test
-    public void createFireStationTestBadRequestAlreadyExists() throws JsonProcessingException, Exception{
+    public void createFireStationTestBadRequestAlreadyExists() throws JsonProcessingException, Exception {
         FireStation fireStation = new FireStation("3 Rory Rd", 8);
 
-        doThrow(new AlreadyExistsException("Fire station already exists"))
-            .when(fireStationService).createFireStation(any(FireStation.class));
+        mockMvc.perform(post("/firestation")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(fireStation)));
 
         mockMvc.perform(post("/firestation")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(fireStation)))
                 .andExpect(status().isBadRequest());
-        
-        verify(fireStationService, times(1)).createFireStation(fireStation);
     }
-    
-    @Test
-    public void updateFireStationTestOk() throws JsonProcessingException, Exception{
-        FireStation fireStation = new FireStation("3 Rory Rd", 8);
 
-        doNothing().when(fireStationService).updateFireStation(any(FireStation.class));
-        
+    @Test
+    public void updateFireStationTestOk() throws JsonProcessingException, Exception {
+        FireStation fireStationBefore = new FireStation("3 Rory Rd", 8);
+        FireStation fireStationAfter = new FireStation("3 Rory Rd", 6);
+
+        mockMvc.perform(post("/firestation")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(fireStationBefore)));
+
         mockMvc.perform(put("/firestation")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(fireStation)))
+                .content(objectMapper.writeValueAsString(fireStationAfter)))
                 .andExpect(status().isOk());
 
-        verify(fireStationService, times(1)).updateFireStation(fireStation);
+        assertTrue(dataRepository.readData().getFireStations().contains(fireStationAfter));
+        assertFalse(dataRepository.readData().getFireStations().contains(fireStationBefore));
     }
 
     @Test
-    public void updateFireStationTestNotFound() throws JsonProcessingException, Exception{
+    public void updateFireStationTestNotFound() throws JsonProcessingException, Exception {
         FireStation fireStation = new FireStation("3 Rory Rd", 8);
-
-        doThrow(new NotFoundException("Fire station not found"))
-            .when(fireStationService).updateFireStation(any(FireStation.class));
 
         mockMvc.perform(put("/firestation")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(fireStation)))
                 .andExpect(status().isNotFound());
-        
-        verify(fireStationService, times(1)).updateFireStation(fireStation);
     }
 
     @Test
-    public void updateFireStationTestByNumberNotFound() throws JsonProcessingException, Exception{
-       
-        doThrow(new NotFoundException("Fire station not found"))
-            .when(fireStationService).deleteFireStation(1);
+    public void deleteFireStationByNumberTestOk() throws JsonProcessingException, Exception {
+        int stationNumber = 8;
+        FireStation fireStation = new FireStation("3 Rory Rd", stationNumber);
+
+        mockMvc.perform(post("/firestation")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(fireStation)));
 
         mockMvc.perform(delete("/firestation")
-            .param("stationNumber","1"))
-            .andExpect(status().isNotFound());
-        
-        verify(fireStationService, times(1)).deleteFireStation(1);
+                .param("stationNumber", String.valueOf(stationNumber)))
+                .andExpect(status().isOk());
+
+        assertFalse(dataRepository.readData().getFireStations().stream()
+                .anyMatch(fStation -> fStation.getStation() == stationNumber));
     }
 
     @Test
-    public void updateFireStationTestByAddressNotFound() throws JsonProcessingException, Exception{
-       
+    public void deleteFireStationTestByNumberNotFound() throws JsonProcessingException, Exception {
+
+        mockMvc.perform(delete("/firestation")
+                .param("stationNumber", "1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void deleteFireStationByAddressTestOk() throws JsonProcessingException, Exception {
         String address = "3 Rory Rd";
-        
-        doThrow(new NotFoundException("Address not found"))
-            .when(fireStationService).deleteFireStation(anyString());
+        FireStation fireStation = new FireStation(address, 8);
+
+        mockMvc.perform(post("/firestation")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(fireStation)));
+        mockMvc.perform(delete("/firestation")
+                .param("address", address))
+                .andExpect(status().isOk());
+
+        assertFalse(dataRepository.readData().getFireStations().stream()
+                .anyMatch(fStation -> fStation.getAddress() == address));
+    }
+
+    @Test
+    public void deleteFireStationTestByAddressNotFound() throws JsonProcessingException, Exception {
+        String address = "3 Rory Rd";
 
         mockMvc.perform(delete("/firestation")
-                        .param("address",address))
+                .param("address", address))
                 .andExpect(status().isNotFound());
-        
-        verify(fireStationService, times(1)).deleteFireStation(address);
     }
 
-    @Test
-    public void deleteFireStationByNumberTestOk() throws JsonProcessingException, Exception{
-        
-        doNothing().when(fireStationService).deleteFireStation(anyInt());
+    // @Test
+    // public void getFireStationOk() throws JsonProcessingException, Exception {
+    // int stationNumber = 1;
 
-        mockMvc.perform(delete("/firestation")
-                .param("stationNumber","1"))
-                .andExpect(status().isOk());
-        
-        verify(fireStationService, times(1)).deleteFireStation(1);
-    }
+    // FireStationResponse response = new FireStationResponse(
+    // Set.of(new FireStationPersonDTO("John", "Doe", "123 Main St",
+    // "111-111-1111"),
+    // new FireStationPersonDTO("Jane", "Doe", "123 Main St", "111-111-5555")),
+    // 2,
+    // 0);
 
-    @Test
-    public void deleteFireStationByAddressTestOk() throws JsonProcessingException, Exception{
-        
-        doNothing().when(fireStationService).deleteFireStation(anyInt());
+    // when(fireStationService.getPersonsByStation(anyInt())).thenReturn(response);
 
-        mockMvc.perform(delete("/firestation")
-                .param("address","3 Rory Rd"))
-                .andExpect(status().isOk());
-        
-        verify(fireStationService, times(1)).deleteFireStation("3 Rory Rd");
-    }
+    // mockMvc.perform(get("/firestation")
+    // .param("stationNumber", "1"))
+    // .andExpect(status().isOk())
+    // .andExpect(content().json(objectMapper.writeValueAsString(response)));
 
-    @Test
-    public void getFireStationOk() throws JsonProcessingException, Exception{
-        int stationNumber = 1;
+    // verify(fireStationService, times(1)).getPersonsByStation(stationNumber);
+    // }
 
-        FireStationResponse response = new FireStationResponse(
-            Set.of(new FireStationPersonDTO("John", "Doe", "123 Main St", "111-111-1111"),
-                new FireStationPersonDTO("Jane", "Doe", "123 Main St", "111-111-5555")),
-            2,
-            0);
+    // @Test
+    // public void phoneAlertTestOk() throws JsonProcessingException, Exception {
+    // int stationNumber = 1;
 
-        when(fireStationService.getPersonsByStation(anyInt())).thenReturn(response);
+    // Set<String> phones = Set.of("123-456-7890", "111-111-1111");
 
-        mockMvc.perform(get("/firestation")
-                        .param("stationNumber", "1"))
-                .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(response)));
+    // when(fireStationService.getPhoneNumbersByStation(anyInt())).thenReturn(phones);
 
-        verify(fireStationService, times(1)).getPersonsByStation(stationNumber);
-    }
+    // mockMvc.perform(get("/phoneAlert")
+    // .param("stationNumber", String.valueOf(stationNumber)))
+    // .andExpect(status().isOk())
+    // .andExpect(content().json(objectMapper.writeValueAsString(phones)));
 
+    // verify(fireStationService, times(1)).getPhoneNumbersByStation(stationNumber);
+    // }
 
+    // @Test
+    // public void childAlertTestOk() throws JsonProcessingException, Exception {
+    // String address = "3 Rory St";
 
-    @Test
-    public void phoneAlertTestOk() throws JsonProcessingException, Exception{
-        int stationNumber = 1;
+    // ChildAlertResponse response = new ChildAlertResponse(
+    // Set.of(new ChildAlertChildDTO("Eric", "Doe", 10),
+    // new ChildAlertChildDTO("Kyle", "Doe", 9)),
+    // Set.of(new Person(
+    // "John", "Doe", "123 Main St", "Springfield", "99999", "555-555-5555",
+    // "john.doe@example.com")));
 
-        Set<String> phones = Set.of("123-456-7890", "111-111-1111");
+    // when(fireStationService.getChildrenByAdress(anyString())).thenReturn(response);
 
-        when(fireStationService.getPhoneNumbersByStation(anyInt())).thenReturn(phones);
+    // mockMvc.perform(get("/childAlert")
+    // .param("address", address))
+    // .andExpect(status().isOk())
+    // .andExpect(content().json(objectMapper.writeValueAsString(response)));
 
-        mockMvc.perform(get("/phoneAlert")
-                        .param("stationNumber", String.valueOf(stationNumber)))
-                .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(phones)));
+    // verify(fireStationService, times(1)).getChildrenByAdress(address);
+    // }
 
-        verify(fireStationService, times(1)).getPhoneNumbersByStation(stationNumber);
-    }
-    
-    @Test
-    public void childAlertTestOk() throws JsonProcessingException, Exception{
-        String address = "3 Rory St";
+    // @Test
+    // public void fireTestOk() throws JsonProcessingException, Exception {
+    // String address = "3 Rory St";
 
-        ChildAlertResponse response = new ChildAlertResponse(
-            Set.of(new ChildAlertChildDTO("Eric", "Doe", 10),
-                new ChildAlertChildDTO("Kyle", "Doe", 9)),
-            Set.of(new Person(
-                "John", "Doe", "123 Main St", "Springfield", "99999", "555-555-5555", "john.doe@example.com")));
+    // FireResponse response = new FireResponse(
+    // Set.of(
+    // new PersonWithMedicalDTO("John", "Doe", "111-111-1111", 30, List.of(""),
+    // List.of("")),
+    // new PersonWithMedicalDTO("Jane", "Doe", "111-111-5555", 28, List.of(""),
+    // List.of(""))),
+    // 1);
 
-        when(fireStationService.getChildrenByAdress(anyString())).thenReturn(response);
+    // when(fireStationService.getPersonsByAddress(anyString())).thenReturn(response);
 
-        mockMvc.perform(get("/childAlert")
-                        .param("address", address))  
-                .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(response)));
+    // mockMvc.perform(get("/fire")
+    // .param("address", address))
+    // .andExpect(status().isOk())
+    // .andExpect(content().json(objectMapper.writeValueAsString(response)));
 
-        verify(fireStationService, times(1)).getChildrenByAdress(address);
-    }
+    // verify(fireStationService, times(1)).getPersonsByAddress(address);
+    // }
 
-    @Test
-    public void fireTestOk() throws JsonProcessingException, Exception{
-        String address = "3 Rory St";
+    // @Test
+    // public void floodTestOk() throws JsonProcessingException, Exception {
+    // List<Integer> stationNumbers = List.of(1);
 
-        FireResponse response = new FireResponse(
-           Set.of(
-            new PersonWithMedicalDTO("John", "Doe", "111-111-1111", 30, List.of(""), List.of("")),
-            new PersonWithMedicalDTO("Jane", "Doe", "111-111-5555", 28, List.of(""), List.of(""))
-           ) 
-        , 1);
+    // List<FloodDTO> floodDTOs = List.of(
+    // new FloodDTO(
+    // 1,
+    // Map.of(
+    // "3 Rory St", Set.of(
+    // new PersonWithMedicalDTO("John", "Doe", "111-111-1111", 30, List.of(""),
+    // List.of("")),
+    // new PersonWithMedicalDTO("Jane", "Doe", "111-111-5555", 28, List.of(""),
+    // List.of(""))))));
 
-        when(fireStationService.getPersonsByAddress(anyString())).thenReturn(response);
+    // when(fireStationService.getHomesByStations(anyList())).thenReturn(floodDTOs);
 
-        mockMvc.perform(get("/fire")
-                        .param("address", address))
-                .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(response)));
+    // mockMvc.perform(get("/flood")
+    // .param("stationNumbers", "1"))
+    // .andExpect(status().isOk())
+    // .andExpect(content().json(objectMapper.writeValueAsString(floodDTOs)));
 
-        verify(fireStationService, times(1)).getPersonsByAddress(address);
-    }
+    // verify(fireStationService, times(1)).getHomesByStations(stationNumbers);
 
-    @Test
-    public void floodTestOk() throws JsonProcessingException, Exception{
-        List<Integer> stationNumbers = List.of(1);
-
-        List<FloodDTO> floodDTOs = List.of(
-            new FloodDTO(
-                1,
-                Map.of(
-                    "3 Rory St", Set.of(
-                        new PersonWithMedicalDTO("John", "Doe", "111-111-1111", 30, List.of(""), List.of("")),
-                        new PersonWithMedicalDTO("Jane", "Doe", "111-111-5555", 28, List.of(""), List.of(""))
-                    )
-                )
-            )
-        );
-
-       when(fireStationService.getHomesByStations(anyList())).thenReturn(floodDTOs);
-
-       mockMvc.perform(get("/flood")
-                        .param("stationNumbers","1"))
-                .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(floodDTOs)));
-
-        verify(fireStationService,times(1)).getHomesByStations(stationNumbers);
-
-    }
+    // }
 
 }
-
